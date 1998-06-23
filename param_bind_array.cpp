6d8d4a2ada4b265c4d17b48191db9b7b792526da
Pablo Jorge Eduardo Rodriguez <rodablo@gmail.com>
1998-06-23 16:29:32 -0300

    Infinitud de cosas.

--- param_bind_array.cpp
+++ param_bind_array.cpp
@@ -6,6 +6,8 @@
 #include "dbo.hxx"
 #include "param.hxx"
 
+#define __OBNDRA__ 0
+
 #if _PEND_
 #pragma message message Los nuevos Params por defecto son NULL
 #endif
@@ -43,12 +45,10 @@ TABind::TABind(TGParam& param, size_t nE)
   : TBind(param)
 {
   _nE      = nE; 
-  // aloca los array's auxiliares
+  // aloca los array's auxiliares (_apIndp & _apLen se DEBEN! inicializar en las classes derivadas)
   _apIndp  = new sb2[nE];  
   _apLen   = new ub2[nE];  
   _apRC    = new ub2[nE]; 
-  //
-  //_curSize = nE;
 }
 
 size_t 
@@ -96,7 +96,7 @@ TABind::get_IsNull(VARIANT& Index, VARIANT_BOOL* retv)
   *retv = IsNull(ValidateIndex(Index)) ? VARIANT_TRUE : VARIANT_FALSE;  
 }
 
-/*******************************************************************************
+/**
 *** TTABind
 ***/
 template <VARTYPE vt, class vT, sword sqlt, class sqlT>
@@ -125,14 +125,27 @@ TTABind<vt, vT, sqlt, sqlT>::TTABind(TGParam& param, size_t nE)
 {
   // aloca el array de los datos
   _ap = new sqlT[nE];
-  //
+  // inicializa _apIndp & _apLen
+  for (int i = 0; i < nE; i++)
+    {
+      _apIndp[i] = -1;                  // inicialmente nulo
+      _apLen[i]  = (ub2)sizeof(sqlT);   // len fijo
+      _apRC[i]   = 0;
+    }
+  // ??
   _curSize = nE;
-  // 
+  // bindea
+#if __OBNDRA__
   CHECK_OCI(::obndra(_Param, (text*)_Param.Name().c_str(), -1,
-		     (ub1*)_ap.get(), (ub4)sizeof(sqlT), sqlt, -1,
-		     _apIndp.get(), _apLen.get(), _apRC.get(),
-		     (ub4)/*20*/nE, &_curSize,
+                     (ub1*)_ap.get(), (ub4)sizeof(sqlT), sqlt, -1,
+                     _apIndp.get(), _apLen.get(), _apRC.get(),
+                     (ub4)0/*20*//*nE*/,0/* &_curSize*/,
+                     (text*)0, -1, -1));
+#else
+  CHECK_OCI(::obndrv(_Param, (text*)_Param.Name().c_str(), -1, 
+		     (ub1*)_ap.get(), (ub4)sizeof(sqlT), sqlt, 0, _apIndp.get(), 
 		     (text*)0, -1, -1));
+#endif
 }
 
 template <VARTYPE vt, class vT, sword sqlt, class sqlT>
@@ -157,29 +170,29 @@ TTABind<vt, vT, sqlt, sqlT>::put_Value(VARIANT& Index, VARIANT& Value)
     {
       // asigna
       if (_Param.IsStrict())
-	{
-	  // es estricto
-	  if (vt != V_VT(&Value))
-	    RAISE_INTERNAL(DBO_E_RUNTIME_PARAM_VALUE_TYPE_MISMATCH_I_I, (int)vt, (int)V_VT(&Value));
-	  // asigna
-	  _ap[pos] = *reinterpret_cast<sqlT*>(&(Value.lVal));
-	}
+        {
+          // es estricto
+          if (vt != V_VT(&Value))
+            RAISE_INTERNAL(DBO_E_RUNTIME_PARAM_VALUE_TYPE_MISMATCH_I_I, (int)vt, (int)V_VT(&Value));
+          // asigna utilizando operator vT ()
+	  _ap[pos] = *reinterpret_cast<vT*>(&Value.lVal);
+	  //_ap[pos] = *static_cast<sqlT*>( static_cast<vT*>(&Value.lVal) );
+        }
       else
-	{
-	  // convierte (si es necesario) y copia
-	  VARIANT v;
-	  VariantInit(&v); //   V_VT(&v) = vt;
-	  CHECK_HRESULT(VariantChangeType(&v, &Value, 0, vt));
-	  // asigna
-	  // _ap[pos] = static_cast<sqlT>(v.lVal);  _ap[pos] = *static_cast<sqlT*>(&(v.lVal));     
-	  _ap[pos] = *reinterpret_cast<sqlT*>(&(v.lVal));
-	  //
-	  VariantClear(&v);
-	}
+        {
+          // convierte (si es necesario) y copia
+          VARIANT v;
+          VariantInit(&v); //   V_VT(&v) = vt;
+          CHECK_HRESULT(VariantChangeType(&v, &Value, 0, vt));
+	  // asigna utilizando operator vT ()	
+	  _ap[pos] = *reinterpret_cast<vT*>(&v.lVal);		
+          //
+          VariantClear(&v);
+        }
       // declara tamaño
-      _apLen[pos] = sizeof(sqlT);
+      // no tiene porque cambiar _apLen[pos] = sizeof(sqlT);
       // saca el nulo
-      _apIndp[pos] = 0;   
+      _apIndp[pos] = 0;
     }
   // notifica
   _Param.SetDirtyFlag();
@@ -199,12 +212,12 @@ TTABind<vt, vT, sqlt, sqlT>::get_Value(VARIANT& Index, VARIANT* retv)
   else
     {
       retv->vt = vt;
-      (*(reinterpret_cast<vT*>(&retv->lVal))) = _ap[pos]; 
-      //*static_cast<vT*>(&retv->lVal) = _ap[pos];
+      // asigna (eventualmente usando operador de conversion)
+      *reinterpret_cast<vT*>(&retv->lVal) = _ap[pos];
     }
 }
 
-/*******************************************************************************
+/**
 *** TStringABind
 ***/
 class TStringABind
@@ -221,9 +234,9 @@ public:
 
 protected: // métodos
 protected: // data 
-  char*  _p;
-  size_t _nE;
-  size_t _length;
+  char*   _p;
+  size_t  _nE;
+  size_t  _length;
   //
   ub4    _curSize;   // i/o
 };
@@ -231,22 +244,32 @@ protected: // data
 TStringABind::TStringABind(TGParam& param, size_t nE, size_t length)
   :  TABind(param, nE)
 {
-  _p  = new char[nE * (length + 1)];
+  _p       = new char[nE * (length + 1)];
   _nE      = nE;
   _curSize = nE;
-  _length  = length + 1;
+  _length  = length + 1; 
+  // inicializa _apIndp & _apLen
+  for (int i = 0; i < nE; i++)
+    {
+      _apIndp[i] = -1;                  // inicialmente nulo
+      _apLen[i]  = (ub2)_length;        // len fijo
+      _apRC[i]   = 0;	
+    }
   //
   AP<char> p(_p);
+  //
+#if __OBNDRA__
   CHECK_OCI(::obndra(_Param, (text*)_Param.Name().c_str(), -1,
-		     (ub1*)_p, (ub4)_length, SQLT_STR, -1,
-		     _apIndp.get(), _apLen.get(), _apRC.get(),
-		     (ub4)/*200*/nE, &_curSize,
-		     (text*)0, -1, -1));
-
-
-//    CHECK_OCI(::obndrv(_Param, (text*)_Param.Name().c_str(), -1, 
-// 		       (ub1*)_p, (ub4)_length, SQLT_STR, 0, _apIndp.get(), 
-// 		       (text*)0, -1, -1));
+                     (ub1*)_p, (ub4)_length, SQLT_STR, -1,
+                     _apIndp.get(), _apLen.get(), _apRC.get(),
+                     (ub4)0/*200 nE*/, 0/*&_curSize*/,
+                     (text*)0, -1, -1));
+#else
+  CHECK_OCI(::obndrv(_Param, (text*)_Param.Name().c_str(), -1, 
+		     (ub1*)_p, (ub4)_length, SQLT_STR, 0, _apIndp.get(), 
+		     (text*)0, -1, -1));			
+#endif
+  //
   p.release();
 }
 
@@ -266,32 +289,36 @@ TStringABind::put_Value(VARIANT& Index, VARIANT& Value)
   else
     {
       if (_Param.IsStrict())
-	{
-	  // es estricto
-	  if (VT_BSTR != V_VT(&Value))
-	    RAISE_INTERNAL(DBO_E_RUNTIME_PARAM_VALUE_TYPE_MISMATCH_I_I, 
-			   (int)VT_BSTR, (int)V_VT(&Value));
-	  // verifica el length
-	  size_t valueLength = SysStringLen(V_BSTR(&Value));
-	  if (valueLength > _length - 1)
-	    RAISE_INTERNAL(DBO_E_RUNTIME_PARAM_STRING_TRUNCATION_I_I, 
-			   int(_length - 1), int(valueLength));
-	  // asigna
-	  lstrcpyn(_p + (pos * _length), ANSI(V_BSTR(&Value)), _length);
-	}
+        {
+          // es estricto
+          if (VT_BSTR != V_VT(&Value))
+            RAISE_INTERNAL(DBO_E_RUNTIME_PARAM_VALUE_TYPE_MISMATCH_I_I, 
+                           (int)VT_BSTR, (int)V_VT(&Value));
+          // verifica el length
+          size_t valueLength = SysStringLen(V_BSTR(&Value));
+          if (valueLength > _length - 1)
+            RAISE_INTERNAL(DBO_E_RUNTIME_PARAM_STRING_TRUNCATION_I_I, 
+                           int(_length - 1), int(valueLength));
+          // asigna
+          lstrcpyn(_p + (pos * _length), ANSI(V_BSTR(&Value)), _length);
+          //
+          _apLen[pos] = (ub2)valueLength;
+        }
       else
-	{
-	  // convierte (si es necesario) y copia
-	  VARIANT v;
-	  VariantInit(&v); 
-	  CHECK_HRESULT(VariantChangeType(&v, &Value, 0, VT_BSTR));
-	  // asigna
-	  lstrcpyn(_p + (pos * _length), ANSI(V_BSTR(&v)), _length);
-	  //
-	  VariantClear(&v);
-	}
+        {
+          // convierte (si es necesario) y copia
+          VARIANT v;
+          VariantInit(&v); 
+          CHECK_HRESULT(VariantChangeType(&v, &Value, 0, VT_BSTR));
+          // asigna
+          lstrcpyn(_p + (pos * _length), ANSI(V_BSTR(&v)), _length);
+          //
+          _apLen[pos] = (ub2)SysStringLen(V_BSTR(&v));
+          //
+          VariantClear(&v);
+        }
       // declara tamaño
-      _apLen[pos] = (ub2)_length;
+      //_apLen[pos] = (ub2)_length;
       // saca el nulo
       _apIndp[pos] = 0; 
     }
@@ -327,14 +354,14 @@ TGParam::BindArray(dboVarType AsType, short ArraySize, VARIANT& StringLength)
       size_t length;
       // determina el length
       if (VT_ERROR != V_VT(&StringLength))
-	{
-	  short _length;
-	  if (!GetShortFromVariant(_length, StringLength))
-	    RAISE_INTERNAL(DBO_E_RUNTIME_PARAM_LENGTH_TYPE_MISMATCH_I, V_VT(&StringLength));
-	  length = _length;
-	}
+        {
+          short _length;
+          if (!GetShortFromVariant(_length, StringLength))
+            RAISE_INTERNAL(DBO_E_RUNTIME_PARAM_LENGTH_TYPE_MISMATCH_I, V_VT(&StringLength));
+          length = _length;
+        }
       else
-	RAISE_INTERNAL(DBO_E_RUNTIME_PARAM_CANT_DETERMINE_STRING_SIZE); 
+        RAISE_INTERNAL(DBO_E_RUNTIME_PARAM_CANT_DETERMINE_STRING_SIZE); 
       //
       _apBind = new TStringABind(*this, ArraySize, length);  
     }
@@ -342,32 +369,33 @@ TGParam::BindArray(dboVarType AsType, short ArraySize, VARIANT& StringLength)
     {
       // solo en String
       if (VT_ERROR != V_VT(&StringLength))
-	RAISE_INTERNAL(DBO_E_RUNTIME_PARAM_STRINGLENGTH_ONLY_IN_STRING);
+        RAISE_INTERNAL(DBO_E_RUNTIME_PARAM_STRINGLENGTH_ONLY_IN_STRING);
       //
       switch (AsType)
-	{
-	case VT_UI1:// Byte 
-	case VT_I2: 
-	  _apBind = new TTABind<VT_I2,   short,  SQLT_INT, short>(*this, ArraySize);  
-	  break; // Integer
-	case VT_I4: 
-	  _apBind = new TTABind<VT_I4,   long,   SQLT_INT, long>(*this, ArraySize);   
-	  break; // Long
-	case VT_R4: 
-	  _apBind = new TTABind<VT_R4,   float,  SQLT_FLT, float>(*this, ArraySize);  
-	  break; // Float
-	case VT_R8: 
-	  _apBind = new TTABind<VT_R8,   double, SQLT_FLT, double>(*this, ArraySize); 
-	  break; // Double
-	case VT_DATE:
-	  _apBind = new TTABind<VT_DATE, DATE,   SQLT_DAT, ODATE>(*this, ArraySize);  
-	  break; // DATE
-	  //  case VT_CY:
-	  //    _apBind = new TTABind<VT_CY,   CY,     SQLT_FLT, OCY>(*this, arraySize);
-	  //break; // CY
-	default:
-	  RAISE_INTERNAL(DBO_E_RUNTIME_PARAM_TYPE_MISMATCH_I, (int)AsType);
-	}  
+        {
+//      case VT_UI1:// Byte 
+        case VT_I2: 
+          _apBind = new TTABind<VT_I2,   short,  SQLT_INT, short>(*this, ArraySize);  
+          break; // Integer
+        case VT_I4: 
+          _apBind = new TTABind<VT_I4,   long,   SQLT_INT, long>(*this, ArraySize);   
+          break; // Long
+        case VT_R4: 
+          _apBind = new TTABind<VT_R4,   float,  SQLT_FLT, float>(*this, ArraySize);  
+          break; // Float
+        case VT_R8: 
+          _apBind = new TTABind<VT_R8,   double, SQLT_FLT, double>(*this, ArraySize); 
+          break; // Double
+        case VT_DATE:
+          _apBind = new TTABind<VT_DATE, DATE,   SQLT_DAT, ODATE>(*this, ArraySize);  
+          break; // DATE
+          //  case VT_CY:
+          //    _apBind = new TTABind<VT_CY,   CY,     SQLT_FLT, OCY>(*this, arraySize);
+          //break; // CY
+        default:
+          RAISE_INTERNAL(DBO_E_RUNTIME_PARAM_TYPE_MISMATCH_I, (int)AsType);
+        }  
     }
 }
+
 
